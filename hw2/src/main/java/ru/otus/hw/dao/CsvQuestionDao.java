dcb8137b59f86f8016d7eb26e08c48379a6f9ee4
polliver <polliver@student.21-school.ru>
2024-06-24 17:39:35 +0300

    add hw1 and stereotype annotation

--- hw2/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw2/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,23 +1,38 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Repository;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Repository
 public class CsvQuestionDao implements QuestionDao {
     private final TestFileNameProvider fileNameProvider;
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
-
-        return new ArrayList<>();
+        try (InputStream inputStream = new ClassPathResource(fileNameProvider.getTestFileName()).getInputStream();
+             Reader reader = new InputStreamReader(inputStream)) {
+            CsvToBeanBuilder<QuestionDto> builder = new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withType(QuestionDto.class)
+                    .withSeparator(';')
+                    .withSkipLines(1);
+            List<QuestionDto> dtoList = builder.build().parse();
+            return dtoList.stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+        } catch (Exception e) {
+            throw new QuestionReadException("Failed to read CSV file", e);
+        }
     }
 }
