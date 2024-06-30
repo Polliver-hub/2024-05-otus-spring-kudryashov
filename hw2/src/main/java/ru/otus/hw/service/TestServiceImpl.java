d10bb82aad8b933075b6c90174e7a79ecb1f5130
polliver <polliver@student.21-school.ru>
2024-06-30 15:27:46 +0300

    add executeTestFor in TestServiceImpl

--- hw2/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw2/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,6 +3,8 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
@@ -19,10 +21,24 @@ public class TestServiceImpl implements TestService {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
         var questions = questionDao.findAll();
-        var testResult = new TestResult(student);
+        TestResult testResult = new TestResult(student);
 
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+        for (Question question : questions) {
+            boolean isAnswerValid = false;
+            String isTrueAnswer = "";
+            ioService.printLine(question.text());
+            for (Answer answer : question.answers()) {
+                ioService.printLine(answer.text());
+                if (answer.isCorrect()) {
+                    isTrueAnswer = answer.text();
+                }
+            }
+            String answerResult = ioService.readStringWithPrompt(
+                    "Select an answer option from the suggested ones." +
+                            " The answer must be in string form and strictly repeat the proposed option:");
+            if (answerResult.equals(isTrueAnswer)) {
+                isAnswerValid = true;
+            }
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
