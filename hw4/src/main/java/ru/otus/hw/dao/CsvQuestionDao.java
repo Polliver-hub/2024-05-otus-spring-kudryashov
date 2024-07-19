f2417499c4e7095f3f04e1a28b10bdc015594221
polliver <polliver@student.21-school.ru>
2024-07-19 14:36:51 +0300

    initial, and add dependency -> shell

---
+++ hw4/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,40 @@
+package ru.otus.hw.dao;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.config.annotations.Profiled;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class CsvQuestionDao implements QuestionDao {
+
+    private final TestFileNameProvider fileNameProvider;
+
+    @Profiled
+    @Override
+    public List<Question> findAll() {
+        try (InputStream inputStream = new ClassPathResource(fileNameProvider.getTestFileName()).getInputStream();
+             Reader reader = new InputStreamReader(inputStream)) {
+            CsvToBeanBuilder<QuestionDto> builder = new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withType(QuestionDto.class)
+                    .withSeparator(';')
+                    .withSkipLines(1);
+            List<QuestionDto> dtoList = builder.build().parse();
+            return dtoList.stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+        } catch (Exception e) {
+            throw new QuestionReadException("Failed to read CSV file", e);
+        }
+    }
+}
