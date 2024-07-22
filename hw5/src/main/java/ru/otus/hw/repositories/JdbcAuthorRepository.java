a63eb7539bf30ad2b629fdb7331310eb48ecdd9a
polliver <polliver@student.21-school.ru>
2024-07-22 23:14:08 +0300

    add repos from authors, genres, books

--- hw5/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
+++ hw5/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -1,33 +1,55 @@
 package ru.otus.hw.repositories;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Author;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class JdbcAuthorRepository implements AuthorRepository {
 
+    private final JdbcOperations jdbc;
+
+    private final NamedParameterJdbcOperations namedParameterJdbcOperations;
+
+    public JdbcAuthorRepository(NamedParameterJdbcOperations namedParameterJdbcOperations) {
+        this.jdbc = namedParameterJdbcOperations.getJdbcOperations();
+        this.namedParameterJdbcOperations = namedParameterJdbcOperations;
+    }
+
     @Override
     public List<Author> findAll() {
-        return new ArrayList<>();
+        return jdbc.query("SELECT id, full_name FROM authors", new AuthorRowMapper());
     }
 
     @Override
     public Optional<Author> findById(long id) {
-        return Optional.empty();
+        try {
+            Author author = namedParameterJdbcOperations.queryForObject(
+                    "SELECT id, full_name FROM authors WHERE id = :id",
+                    new MapSqlParameterSource().addValue("id", id),
+                    new AuthorRowMapper());
+            return Optional.ofNullable(author);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     private static class AuthorRowMapper implements RowMapper<Author> {
 
         @Override
-        public Author mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+        public Author mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            long id = resultSet.getLong("id");
+            String fullName = resultSet.getString("full_name");
+            return new Author(id, fullName);
         }
     }
 }
